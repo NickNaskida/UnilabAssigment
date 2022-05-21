279de4da8516b610fb55fce5ca432f3ee0e75ce6
Nasyida <nikoloz2005@gmail.com>
2022-05-21 22:20:19 +0400

    Booking api, validation, bug fixes

--- app.py
+++ app.py
@@ -4,19 +4,49 @@
 # Project description - service booking system with ability to create and book services.
 # Project reference - further project modifications may include users authentication system and documentation of api.
 
+# შენიშვნა: წინა google ფორმაში მოვნიშნე რომ პირველ კურსზე ვსწავლობ, მაგრამ ეგრე არ არის
+# ვსწავლობ ილია ვეკუას სახელობის 42 სკოლაში და წელს ვამთავრებ 11 კლასს :) ძალიან მინდა მივიღო მეტი გამოცდილება ამ სტაჟირებით.
+
+# requirements.txt
+
+# aniso8601==9.0.1
+# attrs==21.4.0
+# click==8.1.3
+# colorama==0.4.4
+# Flask==2.1.2
+# Flask-RESTful==0.3.9
+# Flask-SQLAlchemy==2.5.1
+# greenlet==1.1.2
+# importlib-metadata==4.11.3
+# itsdangerous==2.1.2
+# Jinja2==3.1.2
+# jsonschema==4.5.1
+# MarkupSafe==2.1.1
+# marshmallow==3.15.0
+# packaging==21.3
+# pyparsing==3.0.9
+# pyrsistent==0.18.1
+# pytz==2022.1
+# six==1.16.0
+# SQLAlchemy==1.4.36
+# Werkzeug==2.1.2
+# zipp==3.8.0
+
+
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
+from marshmallow import Schema, fields, validate, ValidationError, validates
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_SORT_KEYS'] = False
 
 # Initializations
 db = SQLAlchemy(app)  # Database
 api = Api(app)  # API
-marsh = Marshmallow(app)  # Marshmallow
 
 
 # Database models
@@ -29,30 +59,46 @@ class Service(db.Model):
         return f'<Service - {self.name}>'
 
 
-# class Bookings(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     service =
-#
-#     def __repr__(self):
-#         return f'<Service - {self.name}>'
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    service_id = db.Column(db.Integer, db.ForeignKey('service.id', ondelete='RESTRICT'), nullable=False)
+    service = db.relationship("Service", backref=db.backref("service", uselist=False), passive_deletes='all')
+
+    def __repr__(self):
+        return f'<Booking - {self.service}>'
 
 
 # Schemas
-class ServiceSchema(marsh.Schema):
-    class Meta:
-        fields = ("id", "name", "description")
-        model = Service
+class ServiceSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True, validate=validate.Length(min=2, max=100))
+    description = fields.Str(required=True, validate=validate.Length(min=2, max=300))
 
 
 service_schema = ServiceSchema()
 services_schema = ServiceSchema(many=True)
 
 
+class BookingSchema(Schema):
+    id = fields.Int(dump_only=True)
+    service_id = fields.Int(required=True)
+
+    @validates("service_id")
+    def validate_quantity(self, service_id):
+        service = Service.query.get(service_id)
+        if not service:
+            raise ValidationError(f"Service wit id - {service_id} does not exist.")
+
+
+booking_schema = BookingSchema()
+bookings_schema = BookingSchema(many=True)
+
+
 # Views
 class Main(Resource):
     def get(self):
         return {
-            'All services': {
+            'Services': {
                 'docs': 'Get / Add services',
                 'url': '/services',
                 'methods': {
@@ -63,17 +109,58 @@ class Main(Resource):
             'Get service': {
                 'docs': 'Get service from database',
                 'url': 'services/get/<service_id>',
-                'arguments': '<service_id> - Integer'
+                'arguments': '<service_id> - Integer',
+                'methods': {
+                    'GET': 'Return service with <service_id> from database'
+                }
             },
             'Edit service': {
                 'docs': 'Edits service in database',
                 'url': 'services/edit/<service_id>',
-                'arguments': '<service_id> - Integer'
+                'arguments': '<service_id> - Integer',
+                'methods': {
+                    'PUT': 'Returns edited service with <service_id> from database'
+                }
             },
             'Delete service': {
                 'docs': 'Deletes service from database',
                 'url': 'services/delete/<service_id>',
-                'arguments': '<service_id> - Integer'
+                'arguments': '<service_id> - Integer',
+                'methods': {
+                    'DELETE': 'Deletes service with <service_id> from database'
+                }
+            },
+            'Bookings': {
+                'docs': 'Get / Add bookings',
+                'url': '/bookings',
+                'methods': {
+                    'GET': 'Returns all bookings from database',
+                    'POST': 'Adds booking to the database'
+                }
+            },
+            'Get booking': {
+                'docs': 'Get booking from database',
+                'url': 'bookings/get/<booking_id>',
+                'arguments': '<booking_id> - Integer',
+                'methods': {
+                    'GET': 'Return service with <booking_id> from database'
+                }
+            },
+            'Edit booking': {
+                'docs': 'Edits booking in database',
+                'url': 'services/edit/<booking_id>',
+                'arguments': '<booking_id> - Integer',
+                'methods': {
+                    'PUT': 'Returns edited booking with <booking_id> from database'
+                }
+            },
+            'Delete booking': {
+                'docs': 'Deletes booking from database',
+                'url': 'services/delete/<booking_id>',
+                'arguments': '<booking_id> - Integer',
+                'methods': {
+                    'DELETE': 'Deletes booking with <booking_id> from database'
+                }
             }
         }
 
@@ -85,12 +172,22 @@ class Services(Resource):
         return services_schema.dump(services)
 
     def post(self):
-        new_service = Service(
-            name=request.json['name'],
-            description=request.json['description']
-        )
+        try:
+            new_service = Service(
+                name=request.json['name'],
+                description=request.json['description']
+            )
+        except KeyError as e:
+            return {"message": f"KeyError, Missing key: <{e}>"}
+
+        try:
+            service_schema.load({"name": request.json['name'], "description": request.json['description']})
+        except ValidationError as e:
+            return e.messages
+
         db.session.add(new_service)
         db.session.commit()
+
         return service_schema.dump(new_service)
 
 
@@ -104,10 +201,19 @@ class EditService(Resource):
     def put(self, service_id):
         service = Service.query.get_or_404(service_id)
 
-        if 'name' in request.json:
-            service.name = request.json['name']
-        if 'description' in request.json:
-            service.description = request.json['description']
+        try:
+            service_schema.load({"name": request.json['name'], "description": request.json['description']})
+        except ValidationError as e:
+            return e.messages
+
+        try:
+            if 'name' in request.json:
+                service.name = request.json['name']
+
+            if 'description' in request.json:
+                service.description = request.json['description']
+        except KeyError as e:
+            return {"message": f"KeyError, Missing key: <{e}>"}
 
         db.session.commit()
         return service_schema.dump(service)
@@ -124,25 +230,59 @@ class DeleteService(Resource):
 # Booking
 class GetServiceBookings(Resource):
     def get(self):
-        return 'Get all service bookings'
+        bookings = Booking.query.all()
+        return bookings_schema.dump(bookings)
 
     def post(self):
-        return 'Book service'
+        try:
+            new_booking = Booking(
+                service_id=request.json['service_id'],
+            )
+        except KeyError as e:
+            return {"message": f"KeyError, Missing key: <{e}>"}
+
+        try:
+            booking_schema.load({"service_id": request.json['service_id']})
+        except ValidationError as e:
+            return e.messages
+
+        db.session.add(new_booking)
+        db.session.commit()
+
+        return booking_schema.dump(new_booking)
 
 
 class GetServiceBooking(Resource):
-    def get(self, service_id):
-        return 'Get service booking'
+    def get(self, booking_id):
+        booking = Booking.query.get_or_404(booking_id)
+        return booking_schema.dump(booking)
 
 
 class EditServiceBooking(Resource):
-    def put(self, service_id):
-        return 'Edit service booking'
+    def put(self, booking_id):
+        booking = Booking.query.get_or_404(booking_id)
+
+        try:
+            if 'service_id' in request.json:
+                booking.service_id = request.json['service_id']
+        except KeyError as e:
+            return {"message": f"KeyError, Missing key: <{e}>"}
+
+        try:
+            booking_schema.load({"service_id": request.json['service_id']})
+        except ValidationError as e:
+            return e.messages
+
+        db.session.commit()
+        return booking_schema.dump(booking)
 
 
 class DeleteServiceBooking(Resource):
-    def delete(self, service_id):
-        return 'Delete service booking'
+    def delete(self, booking_id):
+        booking = Booking.query.get_or_404(booking_id)
+        db.session.delete(booking)
+        db.session.commit()
+        return '', 204
 
 
 api.add_resource(Main, '/')
@@ -151,9 +291,9 @@ api.add_resource(GetService, '/services/get/<int:service_id>')
 api.add_resource(EditService, '/services/edit/<int:service_id>')
 api.add_resource(DeleteService, '/services/delete/<int:service_id>')
 api.add_resource(GetServiceBookings, '/bookings')
-api.add_resource(GetServiceBooking, '/bookings/get/<int:service_id>')
-api.add_resource(EditServiceBooking, '/bookings/edit/<int:service_id>/')
-api.add_resource(DeleteServiceBooking, '/bookings/delete/<int:service_id>')
+api.add_resource(GetServiceBooking, '/bookings/get/<int:booking_id>')
+api.add_resource(EditServiceBooking, '/bookings/edit/<int:booking_id>/')
+api.add_resource(DeleteServiceBooking, '/bookings/delete/<int:booking_id>')
 
 if __name__ == "__main__":
     db.create_all()
